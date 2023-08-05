30bb38396b30e6a7ff3e1cc14c77f8705ebd71d1
emilie <loveskyship@gmail.com>
2023-08-04 20:58:39 -0700

    a function that returns a function

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,27 +1,35 @@
 import { useState } from "react";
 
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
   }
   return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const hello = (who) => () => console.log('hello', who)
+
+  return (
     <>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(' ')}</p>
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
     </>
   )
+
 }
 
 export default App;
