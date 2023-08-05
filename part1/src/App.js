745fde3414c1a43c6809c9124857e3137625dca0
emilie <loveskyship@gmail.com>
2023-08-04 21:00:54 -0700

    function return a function event handler

--- part1/src/App.js
+++ part1/src/App.js
@@ -20,13 +20,17 @@ const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const hello = (who) => () => console.log('hello', who)
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
 
   return (
     <>
-      <button onClick={hello('world')}>button</button>
-      <button onClick={hello('react')}>button</button>
-      <button onClick={hello('function')}>button</button>
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </>
   )
 
