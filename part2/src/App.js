85630ad8ee4a2d0cda1ffe196ff953d568ad0d3c
emilie <loveskyship@gmail.com>
2023-08-05 23:09:58 -0700

    conditional rendering

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,17 +1,37 @@
 import Note from "./components.js/Note"
 import { useState, useEffect } from "react";
 import noteService from "./services/notes"
+import Notification from "./components.js/Notification";
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University Helsinki 2023</em>
+    </div>
+  )
+}
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
       .getAll()
       .then(initialNotes => setNotes(initialNotes))
   }, [])
+
+  if (!notes) {
+    return null
+  }
 
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
@@ -23,7 +43,8 @@ const App = () => {
         setNotes(notes.map(n => n.id !== id ? n : returnedNote))
       })
       .catch(error => {
-        alert(`the note ${note.content} was already deleted from server`)
+        setErrorMessage(`Note ${note.content} was already removed from server`)
+        setTimeout(() => { setErrorMessage(null) }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -52,6 +73,7 @@ const App = () => {
   return (
     <>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -70,6 +92,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type='submit'>save</button>
       </form>
+      <Footer />
     </>
   )
 }
