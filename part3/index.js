cc3c4fa050a55f2b69322adfe3de0fcf20444836
emilie <loveskyship@gmail.com>
2023-08-06 20:18:09 -0700

    Frontend production build

--- part3/index.js
+++ part3/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+app.use(express.static('build'))
+
 const morgan = require('morgan')
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
