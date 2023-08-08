61c764e37d1ab53cc970b3e8203a717bc3c5f4e0
emilie <loveskyship@gmail.com>
2023-08-07 20:00:56 -0700

    Using database in route handlers

--- part3/index.js
+++ part3/index.js
@@ -41,15 +41,29 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+app.post('/api/notes', (request, response) => {
+  const body = request.body
 
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
+  if (body.content === undefined) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
   }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -57,33 +71,6 @@ app.delete('/api/notes/:id', (request, response) => {
   notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
 })
 
 const PORT = process.env.PORT
