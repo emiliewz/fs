3be60646aed59e597bfe0e6e8a7dd8ec38cb7cfa
emilie <loveskyship@gmail.com>
2023-08-27 11:41:12 -0700

    Tests for the Togglable component

--- part5/src/components.js/Note.test.js
+++ part5/src/components.js/Note.test.js
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Note from './Note'
+import Togglable from './Togglable'
 
 test('renders content', () => {
   const note = {
@@ -14,8 +15,7 @@ test('renders content', () => {
 
   // screen.debug()
   const element = screen.getByText('Component testing is done with react-testing-library')
-
-  screen.debug(element)
+  // screen.debug(element)
 
   expect(element).toBeDefined()
 })
@@ -37,4 +37,49 @@ test('clicking the button calls event handler once', async () => {
   await user.click(button)
 
   expect(mockHandler.mock.calls).toHaveLength(1)
+})
+
+describe('<Togglable />', () => {
+  let container
+
+  beforeEach(() => {
+    container = render(
+      <Togglable buttonLabel='show...'>
+        <div className='testDiv' >
+          togglable content
+        </div>
+      </Togglable>
+    ).container
+  })
+
+  test('renders its children', async () => {
+    await screen.findAllByText('togglable content')
+  })
+
+  test('at start the children are not displayed', () => {
+    const div = container.querySelector('.togglableContent')
+    expect(div).toHaveStyle('display:none')
+  })
+
+  test('after clicking the button, children are displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('show...')
+    await user.click(button)
+
+    const div = container.querySelector('.togglableContent')
+    expect(div).not.toHaveStyle('display:none')
+  })
+
+  test('toggled content can be closed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('show...')
+    await user.click(button)
+
+    const closedButton = screen.getByText('cancel')
+    await user.click(closedButton)
+
+    const div = container.querySelector('.togglableContent')
+    expect(div).toHaveStyle('display:none')
+  })
+
 })
