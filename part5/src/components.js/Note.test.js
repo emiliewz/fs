865266742ac0b5635cd6b3a3e67ded0f6feab5dd
emilie <loveskyship@gmail.com>
2023-08-27 11:06:34 -0700

    Debugging tests

--- part5/src/components.js/Note.test.js
+++ part5/src/components.js/Note.test.js
@@ -9,8 +9,12 @@ test('renders content', () => {
     important: true
   }
 
-  const { container } = render(<Note note={note} />)
+  render(<Note note={note} />)
 
-  const div = container.querySelector('.note')
-  expect(div).toHaveTextContent('Component testing is done with react-testing-library')
+  // screen.debug()
+  const element = screen.getByText('Component testing is done with react-testing-library')
+
+  screen.debug(element)
+
+  expect(element).toBeDefined()
 })
