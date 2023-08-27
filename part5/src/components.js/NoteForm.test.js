ae6ef97932439b17d6a0ddf428effff65f2774b6
emilie <loveskyship@gmail.com>
2023-08-27 12:24:49 -0700

    About finding the elements v4

--- part5/src/components.js/NoteForm.test.js
+++ part5/src/components.js/NoteForm.test.js
@@ -8,15 +8,14 @@ test('<NoteForm /> updates parent state and calls onSubmit', async () => {
   const createNote = jest.fn()
   const user = userEvent.setup()
 
-  render(<NoteForm createNote={createNote} />)
+  const { container } = render(<NoteForm createNote={createNote} />)
 
-  const input = screen.getByPlaceholderText('write note content here')
+  const input = container.querySelector('#note-input')
   const sendButton = screen.getByText('save')
 
   await user.type(input, 'testing a form...')
   await user.click(sendButton)
 
-  // console.log(createNote.mock.calls)
   expect(createNote.mock.calls).toHaveLength(1)
   expect(createNote.mock.calls[0][0].content).toBe('testing a form...')
 
