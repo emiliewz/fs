74ee3ec239394f232acbdf961674d1d109a53973
emilie <loveskyship@gmail.com>
2023-08-30 13:08:33 -0700

    Finishing the filters

--- part6/src/main.jsx
+++ part6/src/main.jsx
@@ -14,14 +14,8 @@ const reducer = combineReducers({
 
 const store = createStore(reducer)
 
-console.log(store.getState())
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <div />
+    <App />
   </Provider>
 )
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
