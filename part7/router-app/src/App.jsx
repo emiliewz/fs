0c8b1cbd8ec4a333868417275406473d978e0750
emilie <loveskyship@gmail.com>
2023-09-05 13:24:35 -0700

    React Bootstrap - Tables

--- part7/router-app/src/App.jsx
+++ part7/router-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Routes, Route, Link, Navigate, useParams, useNavigate, useMatch } from 'react-router-dom'
+import { Table } from 'react-bootstrap'
 
 const Home = () => (
   <div>
@@ -19,13 +20,22 @@ const Note = ({ note }) => (
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      )}
-    </ul>
+    <Table striped>
+      <tbody>
+        {notes.map(note =>
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>
+                {note.content}
+              </Link>
+            </td>
+            <td>
+              {note.user}
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
   </div>
 )
 
