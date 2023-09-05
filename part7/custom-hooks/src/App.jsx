c34f475cec227b500f03cc1ef56428eb2a4c804b
emilie <loveskyship@gmail.com>
2023-09-04 20:19:31 -0700

    Custom hooks v1

---
+++ part7/custom-hooks/src/App.jsx
@@ -0,0 +1,22 @@
+import useCounter from './useCounter'
+
+const App = () => {
+  const counter = useCounter()
+
+  return (
+    <div>
+      <div>{counter.value}</div>
+      <button onClick={counter.increase}>
+        plus
+      </button>
+      <button onClick={counter.decrease}>
+        minus
+      </button>
+      <button onClick={counter.zero}>
+        zero
+      </button>
+    </div>
+  )
+}
+
+export default App
