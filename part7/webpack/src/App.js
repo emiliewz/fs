1f8f2a42073c366b541acc3f1ee25ee3fccbad66
emilie <loveskyship@gmail.com>
2023-09-05 18:57:12 -0700

    Polyfill

--- part7/webpack/src/App.js
+++ part7/webpack/src/App.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import './index.css'
 import axios from 'axios'
+import PromisePolyfill from 'promise-polyfill'
+
+if (!window.Promise) {
+  window.Promise = PromisePolyfill
+}
 
 const useNotes = (url) => {
   const [notes, setNotes] = useState([])
