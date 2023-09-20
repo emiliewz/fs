46feccfcef2f1136384dee83e1de996d103d413c
emilie <loveskyship@gmail.com>
2023-09-19 19:03:11 -0700

    Updating a phone number

--- part8/graphql-phonebook-frontend/src/App.jsx
+++ part8/graphql-phonebook-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import PersonForm from './components/PersonForm'
 
 import { ALL_PERSONS } from './queries'
 import { useState } from 'react'
+import PhoneForm from './components/PhoneForm'
 
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
@@ -27,6 +28,7 @@ const App = () => {
       <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
     </div>
   )
 }
