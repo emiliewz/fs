0b5c97f7d86eb5480c18f50214316e39390ad280
emilie <loveskyship@gmail.com>
2023-10-01 11:36:42 -0700

    Fragments

--- part8/graphql-phonebook-frontend/src/queries.js
+++ part8/graphql-phonebook-frontend/src/queries.js
@@ -13,13 +13,19 @@ export const ALL_PERSONS = gql`
 export const FIND_PERSON = gql`
   query findPersonByName($nameToSearch: String!) {
     findPerson(name: $nameToSearch) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
+      ...PersonDetails
+    }
+  }
+  ${PERSON_DETAILS}
+`
+
+const PERSON_DETAILS = gql`
+  fragment PersonDetails on Person {
+    name
+    phone
+    address {
+      street
+      city
     }
   }
 `
