d20f23f5ea675bc093d7cb51c47685ad0832d329
emilie <loveskyship@gmail.com>
2023-10-04 15:05:02 -0700

    Improving the project

--- part9/typescript/multiplier.ts
+++ part9/typescript/multiplier.ts
@@ -1,30 +1,33 @@
-type Operation = 'multiply' | 'add' | 'divide';
+interface MultiplyValues {
+  value1: number;
+  value2: number;
+}
 
-const calculator = (a: number, b: number, op: Operation): number => {
-  switch(op) {
-    case 'multiply':
-      return a* b;
-    case 'divide':
-      if (b === 0) throw new Error('Can\'t divide by 0!');
-      return a / b;
-    case 'add': 
-      return a + b;
-    default:
-      throw new Error('Operation is not multiply, add or divide!');
+const parseArguments = (args: string[]): MultiplyValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))){
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
   }
 }
 
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText,  a * b);
+}
+
 try {
-  console.log(calculator(1, 5, 'divide'));
-} catch(error: unknown) {
-  let errorMessage = 'Something went wrong: '
-  // here we can not use error.message
+  const { value1, value2 } = parseArguments(process.argv);
+  multiplicator(value1, value2, `Multiplied ${value1} and ${value2}, the result is:`);
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
   if (error instanceof Error) {
-    // the type is narrowed and we can refer to error.message
-    errorMessage += error.message;
+    errorMessage += 'Error: ' + error.message;
   }
-  // here we can not use error.message
-  
-  console.log(errorMessage)
-  
+  console.log(errorMessage);
 }
