3b902dffcfd5e655c1f69637dcad85ad0d68462b
emilie <loveskyship@gmail.com>
2023-10-06 22:18:27 -0700

    React components with TypeScript v2

--- part9/react-ts/src/main.tsx
+++ part9/react-ts/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
+interface WelcomeProps {
+  name: string;
+}
+
+const Welcome = ({ name }: { name: string }): JSX.Element => {
+  return <h1>Hello, {name}</h1>
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Welcome name="Sarah" />  
 )
