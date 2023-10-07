fe1aa86b294be201f3dc1ae2d9b4c909422e2480
emilie <loveskyship@gmail.com>
2023-10-07 15:35:19 -0700

    More type narrowing

--- part9/react-ts/src/App.tsx
+++ part9/react-ts/src/App.tsx
@@ -6,51 +6,76 @@ interface CoursePartBase {
 
 interface CoursePartBasic extends CoursePartBase {
   description: string;
-  kind: "basic"
+  kind: 'basic'
 }
 
 interface CoursePartGroup extends CoursePartBase {
   groupProjectCount: number;
-  kind: "group"
+  kind: 'group'
 }
 
 interface CoursePartBackground extends CoursePartBase {
   description: string;
   backgroundMaterial: string;
-  kind: "background"
+  kind: 'background'
 }
 
 type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
 
+/**
+ * Helper function for exhaustive type checking
+ */
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
 const App = () => {
-  const courseName = "Half Stack application development";
+  const courseName = 'Half Stack application development';
   const courseParts: CoursePart[] = [
     {
-      name: "Fundamentals",
+      name: 'Fundamentals',
       exerciseCount: 10,
-      description: "This is an awesome course part",
-      kind: "basic"
+      description: 'This is an awesome course part',
+      kind: 'basic'
     },
     {
-      name: "Using props to pass data",
+      name: 'Using props to pass data',
       exerciseCount: 7,
       groupProjectCount: 3,
-      kind: "group"
+      kind: 'group'
     },
     {
-      name: "Basics of type Narrowing",
+      name: 'Basics of type Narrowing',
       exerciseCount: 7,
-      description: "How to go from unknown to string",
-      kind: "basic"
+      description: 'How to go from unknown to string',
+      kind: 'basic'
     },
     {
-      name: "Deeper type usage",
+      name: 'Deeper type usage',
       exerciseCount: 14,
-      description: "Confusing description",
-      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
-      kind: "background"
+      description: 'Confusing description',
+      backgroundMaterial: 'https://type-level-typescript.com/template-literal-types',
+      kind: 'background'
     },
   ]
+
+  courseParts.forEach(part => {
+    switch (part.kind) {
+      case 'basic':
+        console.log(part.name, part.description, part.exerciseCount)
+        break
+      case 'group':
+        console.log(part.name, part.exerciseCount, part.groupProjectCount)
+        break
+      case 'background':
+        console.log(part.name, part.exerciseCount, part.description, part.backgroundMaterial)
+        break
+      default:
+        return assertNever(part)
+    }
+  })
 
   return (
     <>
